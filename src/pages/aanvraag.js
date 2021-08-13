b4cdd6aa5dcbb295a0eb42d0e240896326ed4924
Victor Laforga <victorlaforga@hotmail.com>
2021-08-13 23:05:57 +0200

    change contact page

--- src/pages/aanvraag.js
+++ src/pages/aanvraag.js
@@ -4,7 +4,7 @@ import "@typeform/embed/build/css/widget.css"; // import necessary CSS
 import Navigation from '../components/section-1/nav';
 import Menu from "../components/menu"
 import "../styles/styles.scss"
-
+import Vector from "../assets/Vector.svg";
 import { AccessProvider } from  '../contexts/access'
 
 const MyTypeformEmbed = () => {
@@ -15,15 +15,36 @@ const MyTypeformEmbed = () => {
     createWidget("CWcJdwc7", { container: container.current });
   }, []);
 
-  return (<div>
-    
+  return (<div className="contact-container">
+
     <div className="inquiry-container">
       
       <AccessProvider>
       <Navigation open={open} toggleMenu={() => setOpen(!open)} />
         <Menu open={open} toggleMenu={() => setOpen(!open)} />
       </AccessProvider>
+      <div className="contact-info">
+              <div className="">
+                <h3>E-mail:</h3>
+                <a href="mailto:info@laforga.nl">info@laforga.nl</a>
+              </div>
+              <br />
+              <div className="">
+                <h3>Telefoonnummer: </h3>{" "}
+                <a href="tel:+31232084022">+31 23 208 4022</a> <br />
+            
+                </div>
+             <div>
+              
+                <a href="https://api.whatsapp.com/send?phone=+31628340312">
+                  Whatsapp: +31628340312
+                </a>
+                </div>
+             
+             
       
+                
+            </div>
     <div ref={container}></div>
     </div>
     </div>
