59dbb4c04dae7da714f77a51c5c03b508c94ff1f
mrsurjeet <86398512+mrsurjeet@users.noreply.github.com>
2021-08-18 23:17:09 +0530

    Update main.js

--- src/components/section-1/main.js
+++ src/components/section-1/main.js
@@ -2,12 +2,44 @@ import  React from "react";
 import { Player } from "@lottiefiles/react-lottie-player";
 import { AccessContext } from "../../contexts/access";
 import lottieJson from "../../assets/laforga-newLottie.json";
-import '../../styles/styles.scss'
+import '../../styles/styles.scss' ;
+
+// import helmet
+import { Helmet } from "react-helmet" ;
+
 const MainSection = () => {
   const [isFirstAccess] = React.useContext(AccessContext);
 
   return (
     <div className="main-section">
+    
+    // add helmet for schema
+      <Helmet>
+        <script type='application/ld+json'> 
+              {
+                "@context": "http://www.schema.org",
+                "@type": "LocalBusiness",
+                "name": "Lafora",
+                "url": "https://www.laforga.nl",
+                "logo": "https://www.laforga.nl/static/black-logo-bb711faf29ccbee53ac9728e857a94f3.png",
+                "description": "Wij maken het gemakkelijk om uw producten online te verkopen. Online zichtbaarheid is nog nooit zo belangrijk geweest. laforga helpt u in alle fasen van de realisatie van uw digitaal project.",
+                "address": {
+                  "@type": "PostalAddress",
+                  "streetAddress": "Batavierenplantsoen 17, 2025CJ Haarlem",
+                  "addressLocality": "Haarlem",
+                  "addressRegion": "Amsterdam",
+                  "postalCode": "2025CJ",
+                  "addressCountry": "Netherland"
+                },
+                "openingHours": "Mo, Tu, We, Th, Fr, Sa, Su -",
+                "contactPoint": {
+                  "@type": "ContactPoint",
+                  "telephone": "+31 23208 4022",
+                  "contactType": "E-mail: info@laforga.nl"
+                }
+              }
+         </script>
+      </Helmet>
       <Player
         autoplay={true}
         play
